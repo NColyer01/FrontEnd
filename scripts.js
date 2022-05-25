f8f5632d1c884c6842815047bb00a66a8b45c545
NColyer01 <93956325+NColyer01@users.noreply.github.com>
2022-05-25 15:49:52 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,16 @@ var request = new XMLHttpRequest()
 request.open('GET', 'https://zj86ft7ood.execute-api.us-west-2.amazonaws.com/prod/randomcharacter', true)
 
 request.onload = function() {
-
+    const randomData = JSON.parse(request.responseText);
+    console.log(randomData);
+    const age = randomData.physicalAttribute.age;
+    const fname = randomData.physicalAttribute.name.firstName;
+    const lname = randomData.physicalAttribute.name.lastName;
+    const fullName = fname + " " + lname;
+    console.log(age);
+    console.log(fullName);
+    const elem = document.createElement('div');
+    const elemText = document.createTextNode('age');
 }
 
 request.send()
